9772cdc1987cccb57c2e46fbc811a3e4a1065a44
fernando <rluis4490@gmail.com>
2019-12-06 18:27:37 -0600

    integrated Query component from React Apollo and show data from query

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+
+import { GET_ALL_RECIPIES } from '../queries';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Home
-    </div>
-  );
-}
+const App = () => (
+  <div className='App'>
+    <h1>Home</h1>
+    <Query query={GET_ALL_RECIPIES}>
+      {({ data, loading, error }) =>{
+        if(loading) return <div>Loading</div>
+        if(error) return <div>Error</div>
+        console.log(data);
+        return (
+          <React.Fragment>
+
+          <p>Recipes</p>
+          {data.getAllRecipes.map(({name,description})=>
+            <div>
+              <span>{name}</span>
+              <span>{description}</span>
+            </div>
+          )}
+          </React.Fragment>
+        )
+      }}
+    </Query>
+  </div>
+);
 
 export default App;
