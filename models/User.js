47502f140e43047113b30bf5bf08c67d5a3f6bb2
fernando <rluis4490@gmail.com>
2019-12-10 15:04:22 -0600

    crypt password for sigup user using middleware schema.pre from mongoose

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -26,4 +27,38 @@ const UserSchema = new Schema({
     }
 });
 
+/*
+bcrypt.genSalt(rounds, cb)
+
+rounds - [OPTIONAL] - 
+the cost of processing the data. (default - 10)
+
+cb - [REQUIRED] - 
+a callback to be fired once the salt has been generated. 
+uses eio making it asynchronous. 
+err - First parameter to the callback detailing any errors. 
+salt - Second parameter to the callback providing the generated salt
+*/
+/* 
+Middleware 
+(also called pre and post hooks) are functions which are passed 
+control during execution of asynchronous functions. 
+Middleware is specified on the schema level and is useful for writing plugins.
+*/
+UserSchema.pre('save',function(next){
+    console.log(this);
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err,salt) =>{
+        if (err) return next(err);
+        
+        bcrypt.hash(this.password,salt,(err, hash)=>{
+            if (err) return next(err);
+            this.password = hash;
+            next();
+        })
+    });
+});
 module.exports = mongoose.model('User',UserSchema);
