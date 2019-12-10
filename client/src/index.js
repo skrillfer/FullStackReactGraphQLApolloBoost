8ef86d62034f78878d0509ddd55c9fd051b8cd35
fernando <rluis4490@gmail.com>
2019-12-10 15:38:33 -0600

    integrated signin and signup component and install react router dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 import './index.css';
+
+import SignIn from './components/Auth/signin.component';
+import SignUp from './components/Auth/signup.component';
 import App from './components/App';
 
 
@@ -11,9 +15,20 @@ const client = new ApolloClient({
     uri: 'http://localhost:4444/graphql'
 });
 
+const Root = () =>(
+    <Router>
+        <Switch>
+            <Route path='/' exact component={App} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Redirect to='/' />
+        </Switch>
+    </Router>
+);
+
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <App />
+        <Root />
     </ApolloProvider>,
     document.getElementById('root')
 );
