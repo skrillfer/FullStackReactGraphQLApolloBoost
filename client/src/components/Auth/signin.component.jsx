de1568681886621098b77126ca0be33497896385
fernando <rluis4490@gmail.com>
2019-12-11 15:45:46 -0600

    Implement SignIn Mutation on Client

--- client/src/components/Auth/signin.component.jsx
+++ client/src/components/Auth/signin.component.jsx
@@ -1,11 +1,75 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
+
+import Error from '../Error/error.component';
+
+import { SIGNIN_USER } from '../../queries/index';
+
+const initialState = {
+    username:'',
+    password:''
+};
 
 class SignIn extends React.Component{
+    state = { ...initialState };
+
+    clearState = () =>{
+        this.setState({ ...initialState });
+    }
+
+    handleChange = event => {
+        const { name, value }  = event.target;
+        this.setState({ [name] : value });
+    }
+
+    handleSubmit = (event, signinUser)  => {
+        event.preventDefault();
+        signinUser().then(data => {
+                console.log(data);
+                this.clearState();
+        }).catch(error=>error);
+        
+    }
 
     render(){
+        const { username, password } = this.state;
         return(
-            <div>
-                SignIn
+            <div className='App'>
+                <h2 className='App'>SignIn</h2>
+                <Mutation mutation={SIGNIN_USER} variables={{ username, password }}>
+                    {( signinUser, { data, loading, error } )=>{
+                        return(
+                            <form className='form' 
+                                    onSubmit = { event => this.handleSubmit(event, signinUser)}
+                            > 
+                                <input 
+                                    type='text' 
+                                    name='username' 
+                                    placeholder='Username'
+                                    onChange = { this.handleChange }
+                                    value = { username }
+                                    required
+                                />
+                                <input 
+                                    type='password' 
+                                    name='password' 
+                                    placeholder='Password'
+                                    onChange = { this.handleChange }
+                                    value = { password }
+                                    required
+                                />
+                                <button 
+                                    disabled = { loading }
+                                    type='submit' 
+                                    className='button-primary'>
+                                    Submit 
+                                </button>
+                                {error && <Error error={ error }/>}
+                            </form>
+                        )
+                    }}
+                    
+                </Mutation>
             </div>
         )
     }
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -31,3 +31,13 @@ mutation($username:String!,$email:String!,$password:String!){
     }
 }
 `;
+
+export const SIGNIN_USER = gql`
+mutation($username:String!,$password:String!){
+    signinUser(
+      username:$username, 
+      password:$password){
+      token
+    }
+}
+`;
